42f3f18f8d95606e5447bf78430a92f8074e56d7
sina morshedi <s.morshedi@hiraparl.com>
2025-06-19 09:43:25 +0300

    Connect FrontEnd to Backend

--- src/main/java/com/example/fastapi/config/WebConfig.java
+++ src/main/java/com/example/fastapi/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:53282/") //
+                        .allowedOrigins("http://localhost:*/") //
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
