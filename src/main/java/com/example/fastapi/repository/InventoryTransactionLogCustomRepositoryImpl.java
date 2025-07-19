f8ffa44ccd721199af9e5cc811b7cc07b2ad2bb4
sina morshedi <s.morshedi@hiraparl.com>
2025-07-19 18:41:36 +0300

    Last Ver OK InventoryTransactionLog response text modify V1.01 /list user info: tested

--- src/main/java/com/example/fastapi/repository/InventoryTransactionLogCustomRepositoryImpl.java
+++ src/main/java/com/example/fastapi/repository/InventoryTransactionLogCustomRepositoryImpl.java
@@ -21,9 +21,6 @@ public class InventoryTransactionLogCustomRepositoryImpl implements InventoryTra
     @Override
     public List<InventoryTransactionResponseDTO> findAllTransactions() {
 
-        // مرحله match در صورت نیاز (اگر بخواهی فیلتر بزنی، فعلاً حذف شده)
-        // AggregationOperation matchAll = Aggregation.match(new Criteria());
-
         AggregationOperation addCarInfoIdObject = context -> new Document("$addFields",
                 new Document("carInfoIdObject", new Document("$toObjectId", "$carInfoId"))
         );
@@ -32,8 +29,8 @@ public class InventoryTransactionLogCustomRepositoryImpl implements InventoryTra
                 new Document("customerIdObject", new Document("$toObjectId", "$customerId"))
         );
 
-        AggregationOperation addUserIdObject = context -> new Document("$addFields",
-                new Document("userIdObject", new Document("$toObjectId", "$userId"))
+        AggregationOperation addCreatorUserIdObject = context -> new Document("$addFields",
+                new Document("creatorUserIdObject", new Document("$toObjectId", "$creatorUserId"))
         );
 
         AggregationOperation addInventoryItemIdObject = context -> new Document("$addFields",
@@ -52,9 +49,9 @@ public class InventoryTransactionLogCustomRepositoryImpl implements InventoryTra
                 .foreignField("_id")
                 .as("customer");
 
-        LookupOperation lookupUser = LookupOperation.newLookup()
+        LookupOperation lookupCreatorUser = LookupOperation.newLookup()
                 .from("users")
-                .localField("userIdObject")
+                .localField("creatorUserIdObject")
                 .foreignField("_id")
                 .as("creatorUser");
 
@@ -66,22 +63,73 @@ public class InventoryTransactionLogCustomRepositoryImpl implements InventoryTra
 
         UnwindOperation unwindCarInfo = Aggregation.unwind("carInfo", true);
         UnwindOperation unwindCustomer = Aggregation.unwind("customer", true);
-        UnwindOperation unwindUser = Aggregation.unwind("creatorUser", true);
+        UnwindOperation unwindCreatorUser = Aggregation.unwind("creatorUser", true);
         UnwindOperation unwindInventoryItem = Aggregation.unwind("inventoryItem", true);
+
+        // تبدیل roleId و permissionId به ObjectId داخل creatorUser
+        AggregationOperation addRolePermissionObjectIds = context -> new Document("$addFields",
+                new Document("creatorUser.roleIdObject", new Document("$toObjectId", "$creatorUser.roleId"))
+                        .append("creatorUser.permissionIdObject", new Document("$toObjectId", "$creatorUser.permissionId"))
+        );
+
+        // Lookup رول و پرمیشن
+        LookupOperation lookupRole = LookupOperation.newLookup()
+                .from("roles")
+                .localField("creatorUser.roleIdObject")
+                .foreignField("_id")
+                .as("creatorUser.role");
+
+        LookupOperation lookupPermission = LookupOperation.newLookup()
+                .from("permissions")
+                .localField("creatorUser.permissionIdObject")
+                .foreignField("_id")
+                .as("creatorUser.permission");
+
+        UnwindOperation unwindRole = Aggregation.unwind("creatorUser.role", true);
+        UnwindOperation unwindPermission = Aggregation.unwind("creatorUser.permission", true);
+
+        // اضافه کردن فیلد userId به صورت رشته (String) برای ارسال به فرانت‌اند
+        AggregationOperation addCreatorUserUserId = context -> new Document("$addFields",
+                new Document("creatorUser.userId", new Document("$toString", "$creatorUser._id"))
+        );
+
+        // پروژه کردن خروجی نهایی
+        ProjectionOperation project = Aggregation.project()
+                .andExpression("_id").as("id")
+                .and("carInfo").as("carInfo")
+                .and("customer").as("customer")
+                .and("creatorUser.userId").as("creatorUser.userId")
+                .and("creatorUser.username").as("creatorUser.username")
+                .and("creatorUser.firstName").as("creatorUser.firstName")
+                .and("creatorUser.lastName").as("creatorUser.lastName")
+                .and("creatorUser.role").as("creatorUser.role")
+                .and("creatorUser.permission").as("creatorUser.permission")
+                .and("inventoryItem").as("inventoryItem")
+                .and("quantity").as("quantity")
+                .and("type").as("type")
+                .and("description").as("description")
+                .and("dateTime").as("dateTime");
 
         Aggregation aggregation = Aggregation.newAggregation(
                 addCarInfoIdObject,
                 addCustomerIdObject,
-                addUserIdObject,
+                addCreatorUserIdObject,
                 addInventoryItemIdObject,
                 lookupCarInfo,
                 unwindCarInfo,
                 lookupCustomer,
                 unwindCustomer,
-                lookupUser,
-                unwindUser,
+                lookupCreatorUser,
+                unwindCreatorUser,
+                addRolePermissionObjectIds,
+                lookupRole,
+                unwindRole,
+                lookupPermission,
+                unwindPermission,
+                addCreatorUserUserId,
                 lookupInventoryItem,
-                unwindInventoryItem
+                unwindInventoryItem,
+                project
         );
 
         AggregationResults<InventoryTransactionResponseDTO> results = mongoTemplate.aggregate(
@@ -90,8 +138,7 @@ public class InventoryTransactionLogCustomRepositoryImpl implements InventoryTra
                 InventoryTransactionResponseDTO.class
         );
 
-        List<InventoryTransactionResponseDTO> list = results.getMappedResults();
-
-        return list;
+        return results.getMappedResults();
     }
+
 }
